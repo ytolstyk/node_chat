2680cf7e3262c168f8ac5d989390108694cced41
App Academy Student <student@appacademy.io>
2014-09-22 14:52:55 -0700

    changing nicknames

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -8,8 +8,24 @@
   };
   
   ChatNamespace.Chat.prototype.sendMessage = function(message) {
-    this.socket.emit('message', {message: message });
+    this.socket.emit('message', { message: message });
   };
+  
+  ChatNamespace.Chat.prototype.parseCommands = function(command) {
+    var path = command.split(" ")[0];
+    var attribute = command.split(" ")[1];
+    switch(path) {
+      case "nick":
+        this.changeNickname(attribute);
+        break;
+      default:
+        this.socket.emit("unrecognizedCommand");
+    }
+  }
+  
+  ChatNamespace.Chat.prototype.changeNickname = function(name) {
+    this.socket.emit("nicknameChangeRequest", name);
+  }
   
 }());
 
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -3,15 +3,23 @@ $(function () {
   var chat = new ChatNamespace.Chat(socket);
   
   socket.on('message', function (data) {
-    $('ul.messages').append('<li>' + data.message + '</li>');
+    $('ul.messages').append('<li>' + data.nickName + " > " + data.message + '</li>');
+  });
+  
+  socket.on("nicknameChangeResult", function(data) {
+    $('ul.messages').append('<li>' + data.nickName + " > " + data.message + '</li>');
   });
   
   $('form').on('submit', function (event) {
     event.preventDefault();
     var $input = $('#message_field');
     var message = $input.val();
+    if (message.substring(0, 1) === '/') {
+      chat.parseCommands(message.substring(1));
+    } else {
+      chat.sendMessage(message);
+    }
     $input.val('');
-    chat.sendMessage(message);
   });
 
 });
